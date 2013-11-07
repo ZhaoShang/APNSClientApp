ef60f91b0e692b4b491bae671e42cc68c4865156
Albert Zhao <albert.zhao@activenetwork.com>
2013-11-07 21:39:06 +0800

    initial commit

---
+++ APNSClientApp/APNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APNAppDelegate.h
+//  APNSClientApp
+//
+//  Created by Albert Zhao on 11/5/13.
+//  Copyright (c) 2013 Jivaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ APNSClientApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  APNSClientApp
+//
+//  Created by Albert Zhao on 11/5/13.
+//  Copyright (c) 2013 Jivaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APNAppDelegate class]));
+    }
+}
